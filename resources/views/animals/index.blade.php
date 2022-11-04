6d589d563c4879e5be035a584a4034accf94225b
ac-png <alicecorry@icloud.com>
2022-11-04 09:13:09 +0000

    Fixing view all page

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -11,16 +11,16 @@
             @forelse ($animals as $animal)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                     <h2 class="font-bold text 2xl">
-                        <a href="{{ route('animals.show', $animal) }}">Name: {{ $animal->name }}</a>
+                        <a href="{{ route('animals.show', $animal) }}">{{ $animal->name }}</a>
                     </h2>
                     <p class="mt-2">
                         Type: {{ $animal->type }}
                     </p>
                     <p class="mt-2">
-                        Notes: {{ Str::limit($animal->notes, 200) }}
+                        Veterinarian: {{ $animal->veterinarian }}
                     </p>
                     <p class="mt-2">
-                        Veterinarian: {{ $animal->veterinarian }}
+                        Notes: {{ Str::limit($animal->notes, 200) }}
                     </p>
                     <span class="block mt-4 text-sm opacity-70">{{ $animal->updated_at->diffForHumans() }}</span>
                 </div>
