4b9f032f1a1fefc208776e9b6d720d2ececa6d74
ac-png <alicecorry@icloud.com>
2022-11-14 14:37:34 +0000

    Implementation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,7 +17,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // Creates 10 new users (using the fake data).
-        User::factory()->times(10)->create();
+        //get the admin role from the role table. Later(line 31) attach this role to a user
+        $role_admin = Role::where('name', 'admin')->first();
+
+        //get the user role from the role table. Later(line 42) attach this role to a user
+        $role_user = Role::where('name', 'user')->first();
+
+        $admin = new User();
+        $admin->name = 'Anne Wright';
+        $admin->email = 'anne@larafest.ie';
+        $admin->password = Hash::make('password');
+        $admin->save();
+
+        // attach the admin role to the user that was created above.
+        $admin->roles()->attach($role_admin);
+
+
+        $user = new User();
+        $user->name = 'John Jones';
+        $user->email = 'joe@larafest.ie';
+        $user->password = Hash::make('password');
+        $user->save();
+        //attach the user role to this user.
+        $user->roles()->attach($role_user);
     }
 }
